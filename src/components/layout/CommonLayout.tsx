f6357a744c3ff53e13a560ff6631135f4bfd34d1
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-06 20:37:22 +0600

    Add Project Folder Structure

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+interface IProps {
+    children: ReactNode
+}
+
+const CommonLayout = ({ children }: IProps) => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            {children}
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default CommonLayout;
---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return (
+        <div>
+            About Component
+        </div>
+    );
+};
+
+export default About;
