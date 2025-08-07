d3955d80ce0c6e40acff21cfe65e06eddcbb8c9b
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-07 21:15:37 +0600

    Integrate React Hook Form with ShadCN

--- src/components/modules/authentication/RegisterForm.tsx
+++ src/components/modules/authentication/RegisterForm.tsx
@@ -1,12 +1,21 @@
 import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
-// import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
 
 const RegisterForm = ({
     className,
     ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
+
+    const form = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <div className="flex flex-col items-center gap-2 text-center">
@@ -92,6 +101,26 @@ const RegisterForm = ({
             </Button>
           </form>
         </Form> */}
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className='border'>
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Username</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="shadcn" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        This is your public display name.
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Submit</Button>                    </form>
+                </Form>
 
                 <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                     <span className="relative z-10 bg-background px-2 text-muted-foreground">
