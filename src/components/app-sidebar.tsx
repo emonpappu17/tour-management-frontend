6d7c055c6928db10f065e004232e4a2d09003970
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-11 15:47:27 +0600

    add: generate routes dynamically form sidear functionality

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -14,35 +14,11 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 import { Link } from "react-router"
+import { adminSidebarItems } from "@/routes/adminSidebarItems"
 
 // This is sample data.
 const data = {
-  navMain: [
-    {
-      title: "Dashboard",
-      url: "#",
-      items: [
-        {
-          title: "Analytics",
-          url: "/admin/analytics",
-        },
-      ],
-    },
-    {
-      title: "Tour Management",
-      url: "#",
-      items: [
-        {
-          title: "Add Tour Type",
-          url: "/admin/add-tour-type",
-        },
-        {
-          title: "Add Tour",
-          url: "/admin/add-tour",
-        },
-      ],
-    },
-  ],
+  navMain: adminSidebarItems
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
