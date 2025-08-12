ca4f708b28c0a8cce0f9482974058dbb7c1f3a23
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-12 14:32:53 +0600

    fix: site logo and name

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -209,7 +209,7 @@ const Footer = () => {
             <div className="mx-auto container space-y-8 px-4 py-16 lg:space-y-16">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
                     <div>
-                        <div className="flex items-center gap-3">
+                        <div >
                             <Logo />
                         </div>
 
