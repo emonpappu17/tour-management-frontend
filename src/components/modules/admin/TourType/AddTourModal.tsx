7133459a688067849f54db333a8cc7de0a5ff8f6
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-12 19:27:52 +0600

    add: tour type modal

--- src/components/modules/admin/TourType/AddTourModal.tsx
+++ src/components/modules/admin/TourType/AddTourModal.tsx
@@ -1,10 +1,81 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { useAddTourTypeMutation } from "@/redux/features/Tour/tour.api"
+import { useState } from "react"
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
+import { toast } from "sonner"
 
-const AddTourModal = () => {
+export function AddTourTypeModal() {
+    const [open, setOpen] = useState(false)
+    const form = useForm();
+    const [addTourType] = useAddTourTypeMutation();
+
+    const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+        try {
+            const res = await addTourType({ name: data.name }).unwrap();
+            if (res.success) {
+                toast.success("Tour Type Added")
+                setOpen(false);
+                form.reset();
+            }
+        } catch (error) {
+            console.log('tour type error==>', error);
+            setOpen(false)
+            toast.error(error.data.message || "Fail to add Tour Type")
+            form.reset();
+        }
+    }
+
     return (
-        <div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <form>
+                <DialogTrigger asChild>
+                    <Button >Add Tour Type</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Add Tour Type</DialogTitle>
+                    </DialogHeader>
+                    <Form {...form}>
+                        <form id="add-tour-type" onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Tour Type Name</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Tour Type Name"
+                                                {...field}
+                                                value={field.value || ""}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </form>
+                    </Form>
 
-        </div>
-    );
-};
-
-export default AddTourModal;
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit" form="add-tour-type">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </form>
+        </Dialog>
+    )
+}
--- src/pages/Admin/AddTourType.tsx
+++ src/pages/Admin/AddTourType.tsx
@@ -1,3 +1,4 @@
+import { AddTourTypeModal } from "@/components/modules/admin/TourType/AddTourModal";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
@@ -5,12 +6,12 @@ import { Trash2 } from "lucide-react";
 
 const AddTourType = () => {
     const { data } = useGetTourTypesQuery(undefined);
-    console.log(data);
     return (
         <div className="w-full max-w-7xl mx-auto px-5">
             <div className="flex justify-between my-8">
                 <h1 className="text-xl font-semibold">Tour Types</h1>
-                <Button>Add Tour Type</Button>
+                {/* <Button>Add Tour Type</Button> */}
+                <AddTourTypeModal></AddTourTypeModal>
             </div>
             <div className="border border-muted rounded-md">
                 <Table >
