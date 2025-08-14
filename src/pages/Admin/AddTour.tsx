86c7c09e674a9738c919f83505ec1dbb00074eeb
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-14 17:54:09 +0600

    add: day picker for startdate and enddate for the tour

--- src/pages/Admin/AddTour.tsx
+++ src/pages/Admin/AddTour.tsx
@@ -1,30 +1,43 @@
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 import { useGetDivisionsQuery } from "@/redux/features/division/division.api";
 import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const AddTour = () => {
-    const { data: tourTypeData } = useGetTourTypesQuery(undefined);
+    const { data: tourTypeData, isLoading: tourTypeLoading } = useGetTourTypesQuery(undefined);
     const { data: divisionData, isLoading: divisionLoading } = useGetDivisionsQuery(undefined);
+
+    // console.log('tourTypeData==>', tourTypeData);
+    // console.log('division data==>', divisionData);
 
     const divisionOptions = divisionData?.map((item: { _id: string, name: string }) => ({
         value: item._id,
         label: item.name
     }))
 
-    console.log(divisionOptions);
-
+    const tourTypeOptions = tourTypeData?.data?.map((item: { _id: string, name: string }) => ({
+        value: item._id,
+        label: item.name
+    }));
 
     const form = useForm({
         defaultValues: {
             title: "",
             division: "",
             tourType: "",
-            description: ""
+            description: "",
+            startDate: "",
+            endDate: ""
         }
     })
 
@@ -62,6 +75,91 @@ const AddTour = () => {
                             <div className="flex gap-5">
                                 <FormField
                                     control={form.control}
+                                    name="startDate"
+                                    render={({ field }) => (
+                                        <FormItem className="flex flex-col flex-1">
+                                            <FormLabel>Date of birth</FormLabel>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={"outline"}
+                                                            className={cn(
+                                                                "w-full pl-3 text-left font-normal",
+                                                                !field.value && "text-muted-foreground"
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, "PPP")
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={field.value}
+                                                        onSelect={field.onChange}
+                                                        disabled={(date) =>
+                                                            date > new Date() || date < new Date("1900-01-01")
+                                                        }
+                                                        captionLayout="dropdown"
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="endDate"
+                                    render={({ field }) => (
+                                        <FormItem className="flex flex-col flex-1">
+                                            <FormLabel>Date of birth</FormLabel>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant={"outline"}
+                                                            className={cn(
+                                                                "w-full pl-3 text-left font-normal",
+                                                                !field.value && "text-muted-foreground"
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, "PPP")
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0" align="start">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={field.value}
+                                                        onSelect={field.onChange}
+                                                        disabled={(date) =>
+                                                            date > new Date() || date < new Date("1900-01-01")
+                                                        }
+                                                        captionLayout="dropdown"
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            <div className="flex gap-5">
+                                <FormField
+                                    control={form.control}
                                     name="division"
                                     render={({ field }) => (
                                         <FormItem className="flex-1">
@@ -87,26 +185,51 @@ const AddTour = () => {
                                         </FormItem>
                                     )}
                                 />
-
                                 <FormField
                                     control={form.control}
                                     name="tourType"
                                     render={({ field }) => (
                                         <FormItem className="flex-1">
                                             <FormLabel>Tour Type</FormLabel>
-                                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <Select
+                                                onValueChange={field.onChange}
+                                                defaultValue={field.value}
+                                                disabled={tourTypeLoading}
+                                            >
                                                 <FormControl>
                                                     <SelectTrigger className="w-full">
-                                                        <SelectValue />
+                                                        <SelectValue placeholder="Select Tour Type" />
                                                     </SelectTrigger>
                                                 </FormControl>
                                                 <SelectContent>
-                                                    <SelectItem value="m@example.com">m@example.com</SelectItem>
-                                                    <SelectItem value="m@google.com">m@google.com</SelectItem>
-                                                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                                                    {
+                                                        tourTypeOptions?.map((item: { value: string, label: string }) => (
+                                                            <SelectItem
+                                                                key={item.value}
+                                                                value={item.value}
+                                                            >
+                                                                {item.label}
+                                                            </SelectItem>
+                                                        ))
+                                                    }
                                                 </SelectContent>
                                             </Select>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
 
+                            <div className="flex gap-5 items-stretch">
+                                <FormField
+                                    control={form.control}
+                                    name="description"
+                                    render={({ field }) => (
+                                        <FormItem className="flex-1">
+                                            <FormLabel>Description</FormLabel>
+                                            <FormControl>
+                                                <Textarea {...field} className="h-[205px]"></Textarea>
+                                            </FormControl>
                                             <FormMessage />
                                         </FormItem>
                                     )}
