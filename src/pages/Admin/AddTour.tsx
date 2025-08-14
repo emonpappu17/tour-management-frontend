f7ff54448182750f8b9454a2f451e2908147aab9
emonpappu17 <emon.pappu.5@gmail.com>
2025-08-14 18:25:06 +0600

    fix: day picker issues

--- src/pages/Admin/AddTour.tsx
+++ src/pages/Admin/AddTour.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -9,7 +9,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 import { useGetDivisionsQuery } from "@/redux/features/division/division.api";
 import { useGetTourTypesQuery } from "@/redux/features/Tour/tour.api";
-import { format } from "date-fns";
+import { format, formatISO } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
@@ -43,6 +43,13 @@ const AddTour = () => {
 
     const handleSubmit: SubmitHandler<FieldValues> = async (data) => {
         console.log(data);
+        const tourData = {
+            ...data,
+            startDate: formatISO(data.startDate),
+            endDate: formatISO(data.endDate)
+        }
+
+        console.log(tourData);
     }
     return (
         <div className="w-full max-w-4xl mx-auto px-5 mt-16">
@@ -78,7 +85,7 @@ const AddTour = () => {
                                     name="startDate"
                                     render={({ field }) => (
                                         <FormItem className="flex flex-col flex-1">
-                                            <FormLabel>Date of birth</FormLabel>
+                                            <FormLabel>Start Date</FormLabel>
                                             <Popover>
                                                 <PopoverTrigger asChild>
                                                     <FormControl>
@@ -92,7 +99,7 @@ const AddTour = () => {
                                                             {field.value ? (
                                                                 format(field.value, "PPP")
                                                             ) : (
-                                                                <span>Pick a date</span>
+                                                                <span>Pick a start date</span>
                                                             )}
                                                             <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                                         </Button>
@@ -101,10 +108,10 @@ const AddTour = () => {
                                                 <PopoverContent className="w-auto p-0" align="start">
                                                     <Calendar
                                                         mode="single"
-                                                        selected={field.value}
+                                                        selected={new Date(field.value)}
                                                         onSelect={field.onChange}
                                                         disabled={(date) =>
-                                                            date > new Date() || date < new Date("1900-01-01")
+                                                            date < new Date(new Date().setDate(new Date().getDate() - 1))
                                                         }
                                                         captionLayout="dropdown"
                                                     />
@@ -119,7 +126,7 @@ const AddTour = () => {
                                     name="endDate"
                                     render={({ field }) => (
                                         <FormItem className="flex flex-col flex-1">
-                                            <FormLabel>Date of birth</FormLabel>
+                                            <FormLabel>End Date</FormLabel>
                                             <Popover>
                                                 <PopoverTrigger asChild>
                                                     <FormControl>
@@ -133,7 +140,7 @@ const AddTour = () => {
                                                             {field.value ? (
                                                                 format(field.value, "PPP")
                                                             ) : (
-                                                                <span>Pick a date</span>
+                                                                <span>Pick a end date</span>
                                                             )}
                                                             <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                                         </Button>
@@ -142,10 +149,10 @@ const AddTour = () => {
                                                 <PopoverContent className="w-auto p-0" align="start">
                                                     <Calendar
                                                         mode="single"
-                                                        selected={field.value}
+                                                        selected={new Date(field.value)}
                                                         onSelect={field.onChange}
                                                         disabled={(date) =>
-                                                            date > new Date() || date < new Date("1900-01-01")
+                                                            date < new Date(new Date().setDate(new Date().getDate() - 1))
                                                         }
                                                         captionLayout="dropdown"
                                                     />
